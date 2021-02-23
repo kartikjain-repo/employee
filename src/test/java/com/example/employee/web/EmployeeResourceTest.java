432144a202e62f12ead01582888383e20b97304b
kartikjain-repo <kj.kartikjain@gmail.com>
2021-02-23 07:06:34 +0100

    Added SONAR and some config updates

--- src/test/java/com/example/employee/web/EmployeeResourceTest.java
+++ src/test/java/com/example/employee/web/EmployeeResourceTest.java
@@ -10,28 +10,25 @@ import com.example.employee.model.State;
 import com.example.employee.service.EmployeeService;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-//@ExtendWith(SpringExtension.class)
-@WebMvcTest(EmployeeResource.class)
+@WebMvcTest(EmployeseResource.class)
 @RunWith(SpringRunner.class)
-//@OverrideAutoConfiguration(enabled=true)
 public class EmployeeResourceTest {
 
 	@Autowired
